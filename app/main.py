a466b1efa787150c015b23378a3307092281d802
pmdasilva <paulo.estudante9904@gmail.com>
2026-04-26 11:01:01 -0300

    :sparkles: feat: funcionalidade delete 1.0v adicionada

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import crud.read
 import crud.insert
 import crud.update
+import crud.delete
 
 crud.read.show_menu()
 crud.insert.adding_hamburguers()
 crud.insert.list_humburguers()
 crud.update.update_list_hamburguers()
-crud.update.update_list_hamburguers()
+crud.delete.delete_hamburguers()
